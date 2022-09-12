01d0c849d4a96bb700b58a9e6f60fa025ed62819
tanishq1405 <pariwala.tanishq@gmail.com>
2022-09-13 01:03:23 +0530

    day12_complete

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,18 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "iPhone 12 pro",
+        desc: "Apple iPhone 12th gen",
+        price: 999,
+        color: "#33505a",
+        image:
+            "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQiI2_sitOTVNyrr1lSNlhz5HUGMleuoXuESosa-p8KW9YDCZyO4YPQJ2eSgZllyC6GJA2e0qqdDxpKANOVjEqjUflfosEd2A"),
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -14,13 +27,3 @@ class Item {
       required this.color,
       required this.image});
 }
-
-final products = [
-  Item(
-      id: "tan001",
-      name: "iPhone 12 pro",
-      desc: "Apple iPhone 12th gen",
-      price: 999,
-      color: "#33505a",
-      image: "http://google.com")
-];
